edb1f5e6c7cfa1396bcc83ea65e51c9c5f7fad71
Allan <allan012silva@gmail.com>
2026-05-02 12:47:01 -0300

    Estrutura inicial do projeto TaskForge

---
+++ database/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS tasks (
+                                     id SERIAL PRIMARY KEY,
+                                     title VARCHAR(100),
+    description TEXT,
+    status VARCHAR(30)
+    );
